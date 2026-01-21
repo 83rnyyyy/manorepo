3040dc9754320fb987f5b2107b25e42294392721
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:55:27 -0500

    Update counter.ts

--- src/objects/stations/counter.ts
+++ src/objects/stations/counter.ts
@@ -1,59 +1,43 @@
 // objects/stations/cuttingBoard.ts
 import * as THREE from "three";
 import { Station} from "./station.js";
-
 import { HoldableItem } from "../../utilities/holdableItem.js";
 import { Player } from "../player.js";
 import { PlateItem } from "../recipes/plate.js";
 import { Food } from "../../utilities/food.js";
 
-import { ThreeRenderer } from "../../core/render.js";
-
 export class Counter extends Station {
-  	
-  	public heldItem: HoldableItem| null;
-  	public isPlate: boolean = false;
-   
-  	public prompt(): string {
-      return this.promptText;
-    
-    }
 
-  public override tick(dt: number,  playerWorldPos: THREE.Vector3, player: Player, three: ThreeRenderer): void {
-    if(!this.heldItem){
-      if(player.getHeldItem()){
-        this.promptText = "Hold E to Place on Counter";
-      }
-      else{
-        this.promptText =  "";
-        return;
-      }
-    }  
-    else if(player.getHeldItem()) this.promptText =  "Add Ingredient to Plate"; 
-    else this.promptText =  `Hold E To Pickup ${this.heldItem?.type} From Counter`;
-    super.tick(dt,playerWorldPos, player, three);
+  public heldItem: Food | HoldableItem | null;
+  public isPlate: boolean = false;
+  public prompt(player: Player): string {
+    if(!this.heldItem && player.getHeldItem()) return "Hold E to Place on Counter";
+    else if (!player.getHeldItem() && !this.heldItem) return "";
+    else if(player.getHeldItem() && this.heldItem?.name === 'Plate') return "Add Ingredient to Plate"; 
+    else return `Hold E To Pickup ${this.heldItem?.type} From Counter`;
+    
   }
 
+  protected onBegin() {
+    // optional: start animation/sfx
+  }
 
   protected onComplete(player: Player): void {
-    
     if(this.heldItem){
 		if(this.heldItem instanceof PlateItem && player.getHeldItem()){
-			const ingredient = player.removeHeldItem() as HoldableItem;
+			const ingredient = player.removeHeldItem() as Food;
 			(this.heldItem).addIngredient(ingredient);
 		}
 		else{
 			player.pickup(this.heldItem!);
 			this.heldItem = null;
-			
 		}
 		
     }
     else{
 		if(!player.getHeldItem()) return;
-      this.heldItem = player.placeOn(this.anchor, new THREE.Vector3(0.8, 1.35, 0), this.rotation);
+      this.heldItem = player.placeOn(this.anchor, new THREE.Vector3(0.85, 1.35, 0), this.rotation);
       this.heldItem?.type
-      
     }
     
     // TODO: convert ingredient -> chopped ingredient
