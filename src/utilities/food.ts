7eeb8b949c0d21cc7ff710dcba0a5ef2b2afe5b0
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:59:49 -0500

    Update food.ts

--- src/utilities/food.ts
+++ src/utilities/food.ts
@@ -1,13 +1,14 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../core/render.js";
-import { HoldableItem, ItemType } from "./holdableItem.js";
+import { HoldableItem } from "./holdableItem.js";
+import { Assets } from "./assetManager.js";
 
 export abstract class Food extends HoldableItem{
-    public readonly type: ItemType = 'ingredient' as const;
+    public readonly type = 'ingredient' as const;
     public isChoppable: boolean;
     public isCookable: boolean;
 
-    constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number){
-        super(renderer, object, x, y, z);
+    constructor(renderer: ThreeRenderer ,x:number, y:number, z:number, name: Assets){
+        super(renderer, x, y, z, name);
     }
 }
