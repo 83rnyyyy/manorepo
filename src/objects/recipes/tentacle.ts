01ae3cbf6aa4d4b16859042dfbee1ba6d76a37f1
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:54:19 -0500

    Update tentacle.ts

--- src/objects/recipes/tentacle.ts
+++ src/objects/recipes/tentacle.ts
@@ -1,13 +1,14 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
+import AssetManager from "../../utilities/assetManager.js";
 
 export class TentacleItem extends Food{
-  public readonly name = "octopusTentacle" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Octopus Tentacle" as const;
+  constructor(renderer: ThreeRenderer,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Octopus Tentacle');  
     this.isCookable = false;
-    this.isChoppable =false;
+    this.isChoppable = false;
   }
   
 }
