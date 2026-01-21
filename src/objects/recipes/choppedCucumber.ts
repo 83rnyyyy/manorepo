16a769df47544d4a50bdb8fc0aaf68fa18ca554e
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:50:07 -0500

    Refactor ChoppedCucumberItem class properties

--- src/objects/recipes/choppedCucumber.ts
+++ src/objects/recipes/choppedCucumber.ts
@@ -1,12 +1,11 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
-import { ItemName} from "../../utilities/holdableItem.js";
 
 export class ChoppedCucumberItem extends Food{
-  public readonly name: ItemName = "choppedCucumber" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Chopped Cucumber" as const;
+  constructor(renderer: ThreeRenderer ,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Chopped Cucumber');  
     this.isCookable = false;
     this.isChoppable = false;
   }
