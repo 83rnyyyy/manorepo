16d794d80d7524af1d01586973585a0004e89468
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:50:41 -0500

    Update cucumber.ts

--- src/objects/recipes/cucumber.ts
+++ src/objects/recipes/cucumber.ts
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
+import AssetManager from "../../utilities/assetManager.js";
 
 export class CucumberItem extends Food{
-  public readonly name = "cucumber" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Cucumber" as const;
+  constructor(renderer: ThreeRenderer ,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Cucumber');  
     this.isCookable = false;
     this.isChoppable = true;
   }
