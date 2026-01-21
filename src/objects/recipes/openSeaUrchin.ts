ce529565d885b4650217011b1b762b5b74a9ae17
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:51:24 -0500

    Rename OpenedSeaUrchinItem and update constructor

--- src/objects/recipes/openSeaUrchin.ts
+++ src/objects/recipes/openSeaUrchin.ts
@@ -3,9 +3,9 @@ import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
 
 export class OpenedSeaUrchinItem extends Food{
-  public readonly name = "openedSeaUrchin" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Opened Sea Urchin" as const;
+  constructor(renderer: ThreeRenderer, x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Opened Sea Urchin');  
     this.isCookable = false;
     this.isChoppable = false;
   }
