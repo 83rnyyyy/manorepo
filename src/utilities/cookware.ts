77bdebce09f98a8d876cd7e2109622db4dab0d38
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:59:41 -0500

    Update cookware.ts

--- src/utilities/cookware.ts
+++ src/utilities/cookware.ts
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../core/render.js";
 import { HoldableItem } from "./holdableItem.js";
+import { Assets } from "./assetManager.js";
 
 export abstract class Cookware extends HoldableItem{
     public readonly type = 'cookware' as const;
 
-    constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number){
-        super(renderer, object, x, y, z);
+    constructor(renderer: ThreeRenderer,x:number, y:number, z:number, name: Assets | THREE.Group){
+        super(renderer, x, y, z, name);
     }
 }
