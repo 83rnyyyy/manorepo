315613468ba41e98704a766f78c09ed8a80e7d17
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:56:56 -0500

    Update stove.ts

--- src/objects/stations/stove.ts
+++ src/objects/stations/stove.ts
@@ -4,48 +4,61 @@ import { Station} from "./station.js";
 import { ThreeRenderer } from "../../core/render.js";
 import { Player } from "../player.js";
 import { HoldableItem } from "../../utilities/holdableItem.js";
-import { PotItem } from "../recipes/pot.js";
 import { Controller } from "../../core/controller.js";
 import { Food } from "../../utilities/food.js";
+import { PotItem } from "../recipes/pot.js";
+import RecipeManager from "../recipes/recipeManager.js";
 
 export class Stove extends Station {
     public hasItem = true;
     public heldItem: HoldableItem | null;
-    public cookwareLoc:number[] = [2.7,2.1,-8.69];
+    public cookwareLoc:number[] = [2.7,2.1,-8.69]
   
   
   	public prompt(player:Player): string {
     	if(!player.getHeldItem() && this.hasItem)
-			if(this.heldItem?.name == "pot" && (this.heldItem as PotItem).potState == (this.heldItem as PotItem).uncooked) return "Hold E to cook rice";
+			if(this.heldItem?.name == "Pot" && (this.heldItem as PotItem).potState == (this.heldItem as PotItem).uncooked) return "Hold E to cook rice";
 			else return "Hold E to pick up";
-      	else if(player.getHeldItem() && player.getHeldItem()?.name == "pot" || player.getHeldItem()?.name == "pan") return "Hold E to place Item";
+      	else if(player.getHeldItem() && player.getHeldItem()?.name == "Pot") return "Hold E to place Item";
       	return "Hold E to cook";
   	}
 
-  
+  protected onBegin() {
+    // optional: start cooking animation/sfx
+  }
   
   public override tick(dt: number,
    
     playerWorldPos: THREE.Vector3,
-    
     player: Player,
     three:ThreeRenderer){
-        if(this.heldItem){
-          if(this.heldItem.name == "pot" && (this.heldItem as PotItem).potState == (this.heldItem as PotItem).empty){
-        	if(player.getHeldItem() && player.getHeldItem()!.name != "Rice") return;
-          }
-        }
-        super.tick(dt,  playerWorldPos, player, three);
+		const playerItem = player.getHeldItem();
+		if(!this.heldItem || !playerItem || playerItem?.type !== 'ingredient' || !(playerItem as Food).isCookable)return;
+      
+        // if(
+		// 	this.heldItem.name == "Pot" 
+		// 	&& (this.heldItem as PotItem).potState == (this.heldItem as PotItem).empty 
+		// 	&& (playerItem as Food).isCookable
+		// ) ;
+          
+        
+        
+        
+        
+        super.tick(dt, playerWorldPos, player, three);
     }
-	protected onComplete(player:Player): void {
+	protected onComplete(player:Player, three: ThreeRenderer): void {
 		
 		if(this.hasItem){
-			if(this.heldItem?.name == "pot"){
+			if(this.heldItem?.name == "Pot"){
 				const pot = this.heldItem as PotItem;
 				const playerItem = player.getHeldItem();
-				if(pot.potState == pot.uncooked){
-					console.log("here");
+;				if(pot.potState == pot.uncooked){
+					
 					pot.swapToFilledCooked();
+					const cookedItem = RecipeManager.cookItem(pot.itemInPot!, three);
+					pot.itemInPot?.deleteObject();
+					pot.itemInPot = cookedItem;
 					return;
 				}
 				else if(playerItem && playerItem.type === "ingredient" && !pot.itemInPot && (playerItem as Food).isCookable){
@@ -53,14 +66,12 @@ export class Stove extends Station {
 					pot.itemInPot = player.removeHeldItem() as Food;
 					return;
 				}
-				// else if(pot.potState === pot.cooked){
-				// 	player.pickup(pot.itemInPot!);
-				// 	pot.itemInPot = null;
-				// }
+				else if(pot.potState === pot.cooked){
+					player.pickup(pot.itemInPot!);
+					pot.itemInPot = null;
+				}
 				else {
-					console.log(this.heldItem);
 					player.pickup(this.heldItem!);
-					
 					this.heldItem = null;
 					this.hasItem = false;
 				};
@@ -69,15 +80,9 @@ export class Stove extends Station {
 			
 			}
 		else{
-			
-			if(player.getHeldItem() && !(player.getHeldItem()?.name === "pot" || player.getHeldItem()?.name === "pan")) return;
-			
-			this.heldItem = player.placeOn(this.anchor, new THREE.Vector3(0, 1.65, -1.1), this.rotation);
+			if(player.getHeldItem() && (player.getHeldItem()?.name != "Pot")) return;
+			this.heldItem = player.placeOn(this.anchor, new THREE.Vector3(0, 1.6, -1.3), this.rotation);
 			this.hasItem = true;
 		}
-    
   	}
-	protected override onCancel(three: ThreeRenderer, player: Player): void {
-		
-	}
 }
