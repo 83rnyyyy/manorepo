d65c879ef40a2543b35469eb88c94e4fcd588811
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:56:02 -0500

    Update fridge.ts

--- src/objects/stations/fridge.ts
+++ src/objects/stations/fridge.ts
@@ -4,22 +4,19 @@ import { Station } from "./station.js";
 
 import { ThreeRenderer } from "../../core/render.js";
 import { Player } from "../player.js";
-
 import { FridgeItem, FridgeMenu } from "../../utilities/fridgeMenu.js";
-import { RiceItem } from "../recipes/rice.js";
 import { SalmonFishItem } from "../recipes/salmonFish.js";
-import AssetManager from "../../utilities/assetManager.js";
 import { OctopusItem } from "../recipes/octopus.js";
 import { SeaweedItem } from "../recipes/seaweed.js";
 import { ClosedSeaUrchinItem } from "../recipes/closedSeaUrchin.js";
-
 import { CucumberItem } from "../recipes/cucumber.js";
+import {UncookedRiceItem } from "../recipes/uncookedRice.js";
 
 export class Fridge extends Station {
-  private suppressPrompt:boolean = false;
+  private suppressPrompt = false;
   private menu: FridgeMenu;
 
-  private player: Player | null;
+  private player: Player | null = null;
   private three: ThreeRenderer;
 
   private items: FridgeItem[] = [
@@ -29,6 +26,7 @@ export class Fridge extends Station {
     {id: "Octopus", iconSrc: "/public/octopus.png"},
     {id: "Seaweed", iconSrc: "/public/seaweed.png"},
     {id: "Cucumber", iconSrc: "/public/cucumber.png"},
+
   ];
 
   constructor(
@@ -46,33 +44,27 @@ export class Fridge extends Station {
 
       if (picked && this.player) {
         if (picked === "Rice") {
-          const obj = AssetManager.create("Rice");
-          const item = new RiceItem(this.three, obj, 0, 0, 0);
+          const item = new UncookedRiceItem(this.three, 0, 0, 0);
           this.player.pickup(item);
         }
         else if (picked === "Salmon") {
-          const obj = AssetManager.create('Salmon Fish');
-          const item = new SalmonFishItem(this.three, obj, 0,0,0);
+          const item = new SalmonFishItem(this.three, 0,0,0);
           this.player.pickup(item);
         }
         else if(picked === "Octopus"){
-            const obj = AssetManager.create('Octopus');
-            const item = new OctopusItem(this.three, obj, 0,0,0);
+            const item = new OctopusItem(this.three, 0,0,0);
             this.player.pickup(item);
         }
         else if(picked == "Seaweed"){
-            const obj = AssetManager.create('Seaweed');
-            const item = new SeaweedItem(this.three, obj, 0,0,0);
+            const item = new SeaweedItem(this.three, 0,0,0);
             this.player.pickup(item);
         }
         else if(picked == "SeaUrchin"){
-            const obj = AssetManager.create('Closed Sea Urchin');
-            const item = new ClosedSeaUrchinItem(this.three, obj, 0,0,0);
+            const item = new ClosedSeaUrchinItem(this.three,0,0,0);
             this.player.pickup(item);
         }
         else if(picked == "Cucumber"){
-          const obj = AssetManager.create('Cucumber');
-          const item = new CucumberItem(this.three,obj,0,0,0);
+          const item = new CucumberItem(this.three,0,0,0);
           this.player.pickup(item);
         }
       
@@ -81,32 +73,43 @@ export class Fridge extends Station {
   }
 
   // capture current player each frame + block prompt/opening when holding
-  public override tick(dt: number, playerWorldPos: THREE.Vector3, player: Player, three: ThreeRenderer): void {
+  public override tick(
+    dt: number,
+    
+    playerWorldPos: THREE.Vector3,
+   
+    player: Player,
+    three: ThreeRenderer
+  ) {
     this.player = player;
 
     const inside = this.containsPoint(playerWorldPos);
     const holdingItem = player.getHeldItem() !== null;
 
-    
+    // hide prompt when holding item near fridge OR when menu open
     this.suppressPrompt = (inside && holdingItem) || this.menu.isOpen();
 
-    
+    // if menu is open, don't run station cancel/progress logic
     if (this.menu.isOpen()) return;
 
-    
+    // prevent starting fridge if holding something
     if (holdingItem) {
       super.cancel(three, player);
       return;
     }
 
-   
-    super.tick(dt,  playerWorldPos, player, three);
+    // normal station behaviour (progress bar -> onComplete)
+    super.tick(dt, playerWorldPos, player, three);
   }
 
   public prompt(): string {
     if (this.suppressPrompt) return "";
     return "Press E to open fridge";
   }
+
+  
+
+  
 
   protected override onComplete( player: Player, _three: ThreeRenderer): void {
     this.suppressPrompt = true;
