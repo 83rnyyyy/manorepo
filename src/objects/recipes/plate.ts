f299c61adb6f0cb0b7327d44484ef05a432a8f96
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:51:49 -0500

    Update plate.ts

--- src/objects/recipes/plate.ts
+++ src/objects/recipes/plate.ts
@@ -3,56 +3,79 @@ import * as THREE from "three";
 import { HoldableItem } from "../../utilities/holdableItem.js";
 import { ThreeRenderer } from "../../core/render.js";
 import { Cookware } from "../../utilities/cookware.js";
+import RecipeManager from "./recipeManager.js";
+import { Food } from "../../utilities/food.js";
+import AssetManager, { Assets } from "../../utilities/assetManager.js";
 
 export class PlateItem extends Cookware{
-	public readonly name = "plate" as const;
-	public heldIngredients: HoldableItem[] = [];
+	public readonly name = "Plate" as const;
+	public heldIngredients: Food[] = [];
+	private crafting = false;
 	private ingredientSocket = new THREE.Object3D();
 	private slots = [
-		new THREE.Vector3(0.00, 0.05, 0.00),
+		new THREE.Vector3(0.00, 0.1, 0.00),
 		new THREE.Vector3(0.12, 0.05, 0.00),
 		new THREE.Vector3(-0.12, 0.05, 0.00),
 		new THREE.Vector3(0.00, 0.05, 0.12),
 		new THREE.Vector3(0.00, 0.05, -0.12),
 	];
-	constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-		super(renderer,object,x,y,z); 
-		this.object.add(this.ingredientSocket);
+	constructor(renderer: ThreeRenderer,x:number, y:number, z:number) {
+		super(renderer,x,y,z, 'Plate'); 
+		this.object.add(this.ingredientSocket); 
     	this.ingredientSocket.position.set(0, 0, 0);
  
 	}
 	public clearIngredients() {
-		for (const ingredient of this.heldIngredients) {
-			ingredient.object.removeFromParent();
-			ingredient.object.traverse((o: any) => {
-			o.geometry?.dispose?.();
-			const m = o.material;
-			if (Array.isArray(m)) m.forEach((x) => x.dispose?.());
-			else m?.dispose?.();
-			});
+		for (let ingredient of this.heldIngredients) {
+			ingredient.deleteObject();
 		}
 		this.heldIngredients.length = 0;
+		this.crafting = false;
 	}
 
-	public addIngredient(item: HoldableItem){
-		const i = this.heldIngredients.length;
-		let slot;
-		if (this.slots[i]){
-			slot = this.slots[i];
-		}
+	public addIngredient(item: Food){
+		if(this.crafting && this.heldIngredients.length >=1) return;
 		else{
-			slot = this.slots[this.slots.length - 1];
-		}
-		item.object.removeFromParent();
-		this.ingredientSocket.add(item.object);
-		item.object.position.copy(slot!);
-		item.object.rotation.set(0, 0, 0);
-		this.heldIngredients.push(item);
+			const i = this.heldIngredients.length;
+			let slot;
+			if (this.slots[i]){
+				slot = this.slots[i];
+			}
+			else{
+				slot = this.slots[this.slots.length - 1];
+			}
+			item.object.removeFromParent();
+			this.ingredientSocket.add(item.object);
+			item.object.position.copy(slot!);
+			item.object.rotation.set(0, 0, 0);
+			this.heldIngredients.push(item);
+			console.log(this.heldIngredients);
+			if(!this.crafting){
+				this.tryAutoCraft();
+			}
+		} 
+		
+		
+		
 	}
 
-	// public checkForRecipe(){
-	// 	for(let i = 0; i < this.heldIngredients.length; i++){
-	// 		if()
-	// 	}
-	// }
+	private tryAutoCraft() {
+    if (this.crafting) return; 
+    if (this.heldIngredients.length < 2) return;
+
+    // build ingredient list
+    const ingredientNames = this.heldIngredients.map((i) => i.name as Assets);
+
+    // ask recipe manager for crafted dish
+    const crafted = RecipeManager.craftFromPlate(ingredientNames, this.renderer);
+    if (!crafted) return;
+	console.log(crafted);
+	// replace ingredients with crafted dish
+    this.clearIngredients();
+	this.crafting = true;
+	this.addIngredient(crafted);
+	
+    
+  }
+
 }
