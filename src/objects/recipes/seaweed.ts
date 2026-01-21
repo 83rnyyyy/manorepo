eac6c48db64a9871b76d4556f2649c8101a2679e
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:54:10 -0500

    Update seaweed.ts

--- src/objects/recipes/seaweed.ts
+++ src/objects/recipes/seaweed.ts
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
+import AssetManager from "../../utilities/assetManager.js";
 
 export class SeaweedItem extends Food{
-  public readonly name = "seaweed" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Seaweed" as const;
+  constructor(renderer: ThreeRenderer,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Seaweed');  
     this.isCookable = false;
     this.isChoppable = false;
   }
