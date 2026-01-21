942bc67c36a87405bd4560fbe7beee5630a0e6cb
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:52:44 -0500

    Refactor RiceItem class constructor and properties
    Refactor RiceItem constructor and update cookable property.

--- src/objects/recipes/rice.ts
+++ src/objects/recipes/rice.ts
@@ -1,15 +1,13 @@
-// objects/items/plateItem.ts
 import * as THREE from "three";
-import { HoldableItem } from "../../utilities/holdableItem.js";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
 
 export class RiceItem extends Food{
   public readonly name = "Rice" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  constructor(renderer: ThreeRenderer ,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Rice');  
     this.isChoppable = false;
-    this.isCookable = true;
+    this.isCookable = false;
   }
 
   
