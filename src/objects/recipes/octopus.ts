b4800703c79c33c5a1e3a27fb3ff791e11fa5ab1
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:50:50 -0500

    Refactor OctopusItem constructor and name property

--- src/objects/recipes/octopus.ts
+++ src/objects/recipes/octopus.ts
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
+import AssetManager from "../../utilities/assetManager.js";
 
 export class OctopusItem extends Food{
-  public readonly name = "octopus" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Octopus" as const;
+  constructor(renderer: ThreeRenderer, x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Octopus');  
     this.isCookable = false;
     this.isChoppable = true;
   }
