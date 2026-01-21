0a3248c34c4103a3a14fecd91ec07b9c3da45a6a
snowmanlam8 <snowmanbobbypants@gmail.com>
2026-01-21 11:50:32 -0500

    Update closedSeaUrchin.ts

--- src/objects/recipes/closedSeaUrchin.ts
+++ src/objects/recipes/closedSeaUrchin.ts
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { ThreeRenderer } from "../../core/render.js";
 import { Food } from "../../utilities/food.js";
+import AssetManager from "../../utilities/assetManager.js";
 
 export class ClosedSeaUrchinItem extends Food{
-  public readonly name = "closedSeaUrchin" as const;
-  constructor(renderer: ThreeRenderer, object: THREE.Object3D ,x:number, y:number, z:number) {
-    super(renderer,object,x,y,z);  
+  public readonly name = "Closed Sea Urchin" as const;
+  constructor(renderer: ThreeRenderer,x:number, y:number, z:number) {
+    super(renderer,x,y,z, 'Closed Sea Urchin');  
     this.isCookable = false;
     this.isChoppable = true;
   }
